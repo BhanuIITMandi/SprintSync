f1b67a60f34df38ae4790f129b2e7980f6b68aab
BhanuIITMandi <103492479+BhanuIITMandi@users.noreply.github.com>
2026-02-26 11:42:24 +0530

    Adding schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+-- USERS TABLE
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    hashed_password TEXT NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'task_status') THEN
+        CREATE TYPE task_status AS ENUM (
+            'TODO',
+            'IN_PROGRESS',
+            'DONE'
+        );
+    END IF;
+END$$;
+
+-- TASKS TABLE
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    status task_status DEFAULT 'TODO',
+    total_minutes INT DEFAULT 0,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
