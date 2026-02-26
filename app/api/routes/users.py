ec19d26d703478d14a233a9c61bb9d6fd0f8b700
BhanuIITMandi <103492479+BhanuIITMandi@users.noreply.github.com>
2026-02-26 13:17:10 +0530

    Adding docker

---
+++ app/api/routes/users.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserOut
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("/", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    # check if email already exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    #LATER  plain password for now (we hash later during auth step)
+    new_user = User(
+        email=user.email,
+        hashed_password=user.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 from app.models import user, task
+from app.api.routes.users import router as users_router
 
 app = FastAPI(title="SprintSync API")
+
+app.include_router(users_router)
 
 
 @app.get("/")
